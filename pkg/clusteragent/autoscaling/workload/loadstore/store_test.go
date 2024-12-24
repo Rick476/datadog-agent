174f30d52d52f899a14fa09b418290f65d0e75a5
Minyi Zhu <minyi.zhu@datadoghq.com>
2024-12-24 13:40:13 -0500

    update query

--- pkg/clusteragent/autoscaling/workload/loadstore/store_test.go
+++ pkg/clusteragent/autoscaling/workload/loadstore/store_test.go
@@ -9,7 +9,6 @@ package loadstore
 
 import (
 	"fmt"
-	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -18,23 +17,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func createSeriesPayload(i int) *gogen.MetricPayload {
-	containerID := fmt.Sprintf("container_id:%d", i)
-	displayContainerName := fmt.Sprintf("display_container_name:%d", i)
+func createSeriesPayload(i int, time_delta int64) *gogen.MetricPayload {
+	containerID := fmt.Sprintf("container_id:%d", 10)
+	containerName := "container_name:container_test"
+	displayContainerName := fmt.Sprintf("display_container_name:pod_%d-container_test", i)
 	namespace := "kube_namespace:test"
 	deployment := "kube_deployment:redis_test"
+	kubeOwnerrefName := "kube_ownerref_name:redis_test"
+	podName := fmt.Sprintf("pod_name:redis_%d", i)
 	payload := gogen.MetricPayload{
 		Series: []*gogen.MetricPayload_MetricSeries{
 			{
-				Metric: "datadog.test.run",
+				Metric: "container.memory.usage",
 				Type:   3, // Gauge
 				Points: []*gogen.MetricPayload_MetricPoint{
 					{
-						Timestamp: time.Now().Unix() - 100, // 100 seconds ago
+						Timestamp: time.Now().Unix() - time_delta, // time_delta seconds ago
 						Value:     float64(i),
 					},
 				},
-				Tags: []string{containerID, displayContainerName, namespace, deployment},
+				Tags: []string{containerID, displayContainerName, namespace, deployment, kubeOwnerrefName, podName, containerName},
 				Resources: []*gogen.MetricPayload_Resource{
 					{
 						Type: "host", Name: "localHost",
@@ -46,11 +48,14 @@ func createSeriesPayload(i int) *gogen.MetricPayload {
 	return &payload
 }
 
-func createSeriesPayload2(i int) *gogen.MetricPayload {
+func createSeriesPayload2(i int, time_delta int64) *gogen.MetricPayload {
 	containerID := fmt.Sprintf("container_id:%d", i)
-	displayContainerName := fmt.Sprintf("display_container_name:%d", i)
+	containerName := "container_name:container_test"
+	displayContainerName := fmt.Sprintf("display_container_name:pod_%d-container_test", i)
 	namespace := "kube_namespace:test"
 	deployment := "kube_deployment:nginx_test"
+	kubeOwnerrefName := "kube_ownerref_name:nginx_test"
+	podName := fmt.Sprintf("pod_name:nginx_%d", i)
 	payload := gogen.MetricPayload{
 		Series: []*gogen.MetricPayload_MetricSeries{
 			{
@@ -58,11 +63,11 @@ func createSeriesPayload2(i int) *gogen.MetricPayload {
 				Type:   3, // Gauge
 				Points: []*gogen.MetricPayload_MetricPoint{
 					{
-						Timestamp: time.Now().Unix() - 100, // 100 seconds ago
+						Timestamp: time.Now().Unix() - time_delta, // time_delta seconds ago
 						Value:     float64(i),
 					},
 				},
-				Tags: []string{containerID, displayContainerName, namespace, deployment},
+				Tags: []string{containerID, displayContainerName, namespace, deployment, kubeOwnerrefName, podName, containerName},
 				Resources: []*gogen.MetricPayload_Resource{
 					{
 						Type: "host", Name: "localHost2",
@@ -77,15 +82,17 @@ func createSeriesPayload2(i int) *gogen.MetricPayload {
 func TestCreateEntitiesFromPayload(t *testing.T) {
 	numPayloads := 10
 	for i := 0; i < numPayloads; i++ {
-		payload := createSeriesPayload(i)
+		payload := createSeriesPayload(i, 100)
 		entities := createEntitiesFromPayload(payload)
 		assert.Equal(t, len(entities), 1)
 		for k, v := range entities {
-			assert.Equal(t, "datadog.test.run", k.LoadName)
-			assert.Equal(t, "localHost", k.Host)
-			assert.Equal(t, strconv.Itoa(i), k.SourceID)
+			assert.Equal(t, "container.memory.usage", k.MetricName)
 			assert.Equal(t, ValueType(i), v.value)
-			assert.Equal(t, "redis_test", k.Deployment)
+			assert.Equal(t, fmt.Sprintf("redis_%d", i), k.PodName)
+			assert.Equal(t, "test", k.Namespace)
+			assert.Equal(t, "redis_test", k.PodOwnerName)
+			assert.Equal(t, "container_test", k.ContainerName)
+			assert.Equal(t, fmt.Sprintf("pod_%d-container_test", i), k.EntityName)
 		}
 	}
 }
@@ -94,33 +101,74 @@ func TestStoreAndPurgeEntities(t *testing.T) {
 	numPayloads := 100
 	store := EntityStore{
 		key2ValuesMap: make(map[uint64]*dataItem),
+		keyAttrTable:  make(map[compositeKey]podList),
 		lock:          sync.RWMutex{},
-		keyAttrTable:  make(map[string]map[string]map[string]map[uint64]struct{}),
 	}
-	for i := 0; i < numPayloads; i++ {
-		payload := createSeriesPayload(i)
-		entities := createEntitiesFromPayload(payload)
-		store.SetEntitiesValues(entities)
-		payload2 := createSeriesPayload2(i)
-		entities2 := createEntitiesFromPayload(payload2)
-		store.SetEntitiesValues(entities2)
+	for _, time_delta := range []int64{100, 85, 80} {
+		for i := 0; i < numPayloads; i++ {
+			payload := createSeriesPayload(i, time_delta)
+			entities := createEntitiesFromPayload(payload)
+			store.SetEntitiesValues(entities)
+			payload2 := createSeriesPayload2(i, time_delta)
+			entities2 := createEntitiesFromPayload(payload2)
+			store.SetEntitiesValues(entities2)
+
+		}
 	}
 	storeInfo := store.GetStoreInfo()
 	assert.Equal(t, 2, len(storeInfo.StatsResults))
-	for _, statsResult := range storeInfo.StatsResults {
+	for id, statsResult := range storeInfo.StatsResults {
 		assert.Equal(t, numPayloads, statsResult.Count)
-		assert.EqualValues(t, 99, statsResult.Max)
-		assert.EqualValues(t, 0, statsResult.Min)
-		assert.EqualValues(t, 49.5, statsResult.Avg)
-		assert.EqualValues(t, 49, statsResult.Medium)
-		assert.EqualValues(t, 98, statsResult.P99)
-		assert.EqualValues(t, 94, statsResult.P95)
+		if id == 0 {
+			assert.Equal(t, "test", statsResult.Namespace)
+			assert.Equal(t, "redis_test", statsResult.PodOwner)
+			assert.Equal(t, "container.memory.usage", statsResult.MetricName)
+		} else {
+			assert.Equal(t, numPayloads, statsResult.Count)
+			assert.Equal(t, "test", statsResult.Namespace)
+			assert.Equal(t, "nginx_test", statsResult.PodOwner)
+			assert.Equal(t, "container.cpu.usage", statsResult.MetricName)
+		}
 	}
-
 	store.purgeInactiveEntities(30 * time.Second)
 	storeInfo = store.GetStoreInfo()
 	for _, statsResult := range storeInfo.StatsResults {
 		assert.Equal(t, 0, statsResult.Count)
 	}
+}
 
+func TestGetMetrics(t *testing.T) {
+	numPayloads := 100
+	store := EntityStore{
+		key2ValuesMap: make(map[uint64]*dataItem),
+		keyAttrTable:  make(map[compositeKey]podList),
+		lock:          sync.RWMutex{},
+	}
+	for _, time_delta := range []int64{100, 85, 80} {
+		for i := 0; i < numPayloads; i++ {
+			payload := createSeriesPayload(i, time_delta)
+			entities := createEntitiesFromPayload(payload)
+			store.SetEntitiesValues(entities)
+			payload2 := createSeriesPayload2(i, time_delta)
+			entities2 := createEntitiesFromPayload(payload2)
+			store.SetEntitiesValues(entities2)
+
+		}
+	}
+	queryResult := store.GetMetricsRaw("container.cpu.usage", "test", "nginx_test", "")
+	assert.Equal(t, 100, len(queryResult.results))
+	for _, podResult := range queryResult.results {
+		assert.Equal(t, 1, len(podResult.ContainerValues))
+		assert.Equal(t, 0, len(podResult.PodLevelValue))
+		for containerName, entityValues := range podResult.ContainerValues {
+			assert.Equal(t, "container_test", containerName)
+			assert.Equal(t, 3, len(entityValues))
+		}
+	}
+
+	emptyQueryResult := store.GetMetricsRaw("container.cpu.usage", "test", "nginx_test", "container_test2")
+	assert.Equal(t, 0, len(emptyQueryResult.results))
+
+	filteredQueryResult := store.GetMetricsRaw("container.memory.usage", "test", "redis_test", "container_test")
+	assert.Equal(t, 100, len(filteredQueryResult.results))
 }
